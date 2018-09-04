ef0164251e4ad30eee9ae8a6ba222aca793033c1
NivethaKarthikeyan <nivethakarthikeyan1995@gmail.com>
2018-09-04 11:36:06 +0530

    test java file changed

--- sa/src/sa/test.java
+++ sa/src/sa/test.java
@@ -1,5 +1,14 @@
 package sa;
-
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.ResourceBundle;
 public class test {
+	public static void main(String args[]) 
+	{
+		 YearMonth ym = YearMonth.now();  
+		    String s = ym.format(DateTimeFormatter.ofPattern("MM yyyy"));  
+		    System.out.println(s);  
+	}
 
 }
